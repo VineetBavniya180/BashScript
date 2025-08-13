0aa0503e035176577e36b01a884054e1cb34c8e1
VineetBavniya180 <vineet.bavniya@clouddrove.com>
2025-08-13 18:41:54 +0530

    adding docker images delete script

---
+++ scheduling.sh
@@ -0,0 +1,23 @@
+#!/bin/bash 
+
+declare -A imagesCount
+
+abs() {
+    local num=$1
+    echo $(( num < 0 ? -num : num ))
+}
+
+
+for i in $( docker images | awk 'NR != 1 { print $1 }'); do 
+    (( imagesCount["$i"]++ ))
+done
+
+for key in "${!imagesCount[@]}"; do
+    if [ ${imagesCount[$key]} -ge 4 ]; then
+        numToDelete=$(abs $(( ${imagesCount[$key]} - 3 ))) 
+        for del in $(docker images | awk 'NR != 1 && $1 == "'"$key"'" { print $1 ":" $2 }' | tail -n $numToDelete ); do
+            echo "Deleting image: $del"
+            docker image rm -f "$del" || echo "Failed to delete image $del"
+        done
+    fi
+done
